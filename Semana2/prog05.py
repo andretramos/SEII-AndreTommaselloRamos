15e8be44062f7e453215c6bab39bfd7964be3bd8
andretramos <andretramos@hotmail.com>
2021-03-15 14:19:31 -0300

    Apaguei teste e subi prog05

---
+++ Semana2/prog05.py
@@ -0,0 +1,25 @@
+
+student = {'name': 'John', 'age': 25, 'courses': ['Math','CompSci']}
+print(student)
+print(student['name'])
+print(student['courses'])
+
+print(student.get('name'))
+print(student.get('phone', 'Not found'))
+
+student['phone'] = '555-5555'
+student['name'] = 'Jane'
+print(student.get('phone', 'Not found'))
+print(student)
+
+student.update({'name': 'Jane', 'age': 26, 'phone': '555-5555'})
+age = student.pop('age')
+print(student)
+print(age)
+
+print(student.keys())
+print(student.values())
+print(student.items())
+
+for key,value in student.items():
+	print(key, value) 
