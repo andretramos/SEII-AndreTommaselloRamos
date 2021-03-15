7938c4f838d6e3b6dd5c9334da0feaf3ac284ec9
andretramos <andretramos@hotmail.com>
2021-03-15 11:48:45 -0300

    Create prog03.py

---
+++ Semana2/prog03.py
@@ -0,0 +1,33 @@
+num = 3
+print(type(num))
+
+num = 3.5
+print(type(num))
+
+print(3 + 2 - 4)
+print(3//2)
+print(3**2)
+print(2%2)
+print(3%2)
+print(4%2)
+print(5%2)
+
+print('\n')
+print(3*(2+1))
+num = 1
+num += 1
+num *= 10
+num = abs(-num)
+print(round((num + 0.75),1))
+print(num == 20)
+print(num != 20)
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1 + num_2)
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
